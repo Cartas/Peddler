00cb31e80294aa7370b331743e88febdfcd4848b
Richard <livesey7@hotmail.co.uk>
2012-08-05 22:41:58 +0200

    Made the coins a bit more noticeable.  Outputs gold amounts after selling.

--- Peddler.lua
+++ Peddler.lua
@@ -7,6 +7,15 @@ local next = next
 
 local ItemsToSell = {}
 
+	-- 110631 -> 11g6s31c
+local function priceToGold(price)
+	local gold = price / 10000
+	local silver = (price % 10000) / 100
+	local copper = (price % 10000) % 100
+
+	return math.floor(gold) .. "g" .. math.floor(silver) .. "s" .. math.floor(copper) .. "c"
+end
+
 local peddler = CreateFrame("Frame", nil, UIParent)
 peddler:RegisterEvent("MERCHANT_SHOW")
 
@@ -14,6 +23,9 @@ local function peddleGoods(self, event, ...)
 	if not next(ItemsToSell) then
 		return
 	end
+
+	local output = "Peddler sold:\n"
+	local total = 0
 
 	for bagNumber = 0, 4 do
 		for slotNumber = 1, GetContainerNumSlots(bagNumber) do
@@ -23,12 +35,25 @@ local function peddleGoods(self, event, ...)
 				local texture = ItemsToSell[itemID]
 				texture:Hide()
 				texture = nil
+				ItemsToSell[itemID] = nil
+
 				UseContainerItem(bagNumber, slotNumber)
+
+				local _, link, _, _, _, _, _, _, _, _, price = GetItemInfo(itemID)
+				local _, amount = GetContainerItemInfo(bagNumber, slotNumber)
+
+				price = price * amount
+
+				total = total + price
+				output = output .. link .. " for " .. priceToGold(price) .. "\n"
 			end
 		end
 	end
 
 	ItemsToSell = {}
+
+	output = output .. "\nFor a total of " .. priceToGold(total)
+	print(output)
 end
 
 peddler:SetScript("OnEvent", peddleGoods)
